4fcc863330a353498b8a332262fe23f15928a3bb
Rafael Sana Montevechio <rafael_sana@hotmail.com>
2021-07-28 21:17:36 -0300

    refactor: :recycle: refactor Input component

--- src/components/ModalAddFood/index.jsx
+++ src/components/ModalAddFood/index.jsx
@@ -1,10 +1,10 @@
 import { Component, createRef } from 'react';
 import { FiCheckSquare } from 'react-icons/fi';
 
-import { Form } from './styles';
 import Modal from '../Modal';
-import Input from '../Input';
+import { Input } from '../Input';
 
+import { Form } from './styles';
 class ModalAddFood extends Component {
   constructor(props) {
     super(props);
@@ -12,7 +12,7 @@ class ModalAddFood extends Component {
     this.formRef = createRef();
   }
 
-  handleSubmit = async data => {
+  handleSubmit = async (data) => {
     const { setIsOpen, handleAddFood } = this.props;
 
     handleAddFood(data);
@@ -42,6 +42,6 @@ class ModalAddFood extends Component {
       </Modal>
     );
   }
-};
+}
 
 export default ModalAddFood;
--- src/components/ModalEditFood/index.jsx
+++ src/components/ModalEditFood/index.jsx
@@ -1,15 +1,16 @@
 import { Component, createRef } from 'react';
+
 import { FiCheckSquare } from 'react-icons/fi';
 
-import { Form } from './styles';
 import Modal from '../Modal';
-import Input from '../Input';
+import { Input } from '../Input';
 
+import { Form } from './styles';
 class ModalEditFood extends Component {
   constructor(props) {
     super(props);
 
-    this.formRef = createRef()
+    this.formRef = createRef();
   }
 
   handleSubmit = async (data) => {
@@ -24,7 +25,11 @@ class ModalEditFood extends Component {
 
     return (
       <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
-        <Form ref={this.formRef} onSubmit={this.handleSubmit} initialData={editingFood}>
+        <Form
+          ref={this.formRef}
+          onSubmit={this.handleSubmit}
+          initialData={editingFood}
+        >
           <h1>Editar Prato</h1>
           <Input name="image" placeholder="Cole o link aqui" />
 
@@ -43,6 +48,6 @@ class ModalEditFood extends Component {
       </Modal>
     );
   }
-};
+}
 
 export default ModalEditFood;
